5cead9f0243b740dbe2c5011543360df8db359c5
Kam Bielawski <kamtb28@gmail.com>
2022-05-22 14:15:53 -0500

    plot .npy files

--- analyze.py
+++ analyze.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot
 
+targetAngles_front = np.load("data/targetAngles_front.npy")
+targetAngles_back = np.load("data/targetAngles_back.npy")
+matplotlib.pyplot.plot(targetAngles_front, label="front leg target angles")
+matplotlib.pyplot.plot(targetAngles_back, label="back leg target angles")
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
+exit()
+
 backLegSensorValues = np.load("data/backLegSensor.npy")
 frontLegSensorValues = np.load("data/frontLegSensor.npy")
 
