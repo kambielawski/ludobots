cc4a668361863f7c68f1b38336eec6d58b690261
Kam Bielawski <kamtb28@gmail.com>
2022-05-22 14:16:15 -0500

    add sinusoidal motors to leg joints

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet as p
 import pybullet_data
 import time
 import numpy as np
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -25,6 +26,20 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
+amplitude_front = (np.pi/4.0)
+frequency_front = 10
+phaseOffset_front = 0
+
+amplitude_back = (np.pi/4.0)
+frequency_back = 10
+phaseOffset_back = np.pi / 4
+
+targetAngles_front = np.sin(np.linspace(0, 2*np.pi, 1000) * frequency_front + phaseOffset_front)*amplitude_front
+targetAngles_back = np.sin(np.linspace(0, 2*np.pi, 1000) * frequency_back + phaseOffset_back)*amplitude_back
+# np.save("./data/targetAngles_front.npy", targetAngles_front)
+# np.save("./data/targetAngles_back.npy", targetAngles_back)
+# exit()
+
 for i in range(1000):
     if i%100 == 0:
         print(i)
@@ -35,6 +50,24 @@ for i in range(1000):
     # sensor value from robotId
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    # simulate a motor for a particular joint
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName="Torso_BackLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=targetAngles_back[i],
+        # max force calculated in Newton*meters
+        maxForce=50
+    )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName="Torso_FrontLeg",
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=targetAngles_front[i],
+        # max force calculated in Newton*meters
+        maxForce=50
+    )
 
 np.save("./data/backLegSensor.npy", backLegSensorValues)
 np.save("./data/frontLegSensor.npy", frontLegSensorValues)
