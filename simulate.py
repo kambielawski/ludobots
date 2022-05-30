06172ec0e7da241085453659f646f81e8baad1ee
Kam Bielawski <kamtb28@gmail.com>
2022-05-30 16:56:38 -0500

    move constant values to constants.py

--- simulate.py
+++ simulate.py
@@ -5,6 +5,8 @@ import time
 import numpy as np
 import random
 
+import constants as c
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -16,31 +18,23 @@ robotId = p.loadURDF("body.urdf")
 # load box specified by box.sdf into the world
 p.loadSDF("world.sdf")
 
-# add gravitational force to world
-p.setGravity(0,0,-9.8)
+# add gravitational force to world (x,y,z)
+p.setGravity(0,0,c.GRAVITY_FORCE)
 
 # this gets the simulation ready to read values from sensors
 pyrosim.Prepare_To_Simulate(robotId)
 
 # vectors for storing sensor values
-backLegSensorValues = np.zeros(1000)
-frontLegSensorValues = np.zeros(1000)
+backLegSensorValues = np.zeros(c.TIMESTEPS)
+frontLegSensorValues = np.zeros(c.TIMESTEPS)
 
-amplitude_front = (np.pi/4.0)
-frequency_front = 10
-phaseOffset_front = 0
-
-amplitude_back = (np.pi/4.0)
-frequency_back = 10
-phaseOffset_back = np.pi / 4
-
-targetAngles_front = np.sin(np.linspace(0, 2*np.pi, 1000) * frequency_front + phaseOffset_front)*amplitude_front
-targetAngles_back = np.sin(np.linspace(0, 2*np.pi, 1000) * frequency_back + phaseOffset_back)*amplitude_back
+targetAngles_front = np.sin(np.linspace(0, 2*np.pi, c.TIMESTEPS) * c.FREQUENCY_FRONT + c.PHASE_OFFSET_FRONT)*c.AMPLITUDE_FRONT
+targetAngles_back = np.sin(np.linspace(0, 2*np.pi, c.TIMESTEPS) * c.FREQUENCY_BACK + c.PHASE_OFFSET_BACK)*c.AMPLITUDE_BACK
 # np.save("./data/targetAngles_front.npy", targetAngles_front)
 # np.save("./data/targetAngles_back.npy", targetAngles_back)
 # exit()
 
-for i in range(1000):
+for i in range(c.TIMESTEPS):
     if i%100 == 0:
         print(i)
     # sleep for 1/60 of a second
@@ -58,7 +52,7 @@ for i in range(1000):
         controlMode=p.POSITION_CONTROL,
         targetPosition=targetAngles_back[i],
         # max force calculated in Newton*meters
-        maxForce=50
+        maxForce=c.MAX_FORCE
     )
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,
@@ -66,7 +60,7 @@ for i in range(1000):
         controlMode=p.POSITION_CONTROL,
         targetPosition=targetAngles_front[i],
         # max force calculated in Newton*meters
-        maxForce=50
+        maxForce=c.MAX_FORCE
     )
 
 np.save("./data/backLegSensor.npy", backLegSensorValues)
