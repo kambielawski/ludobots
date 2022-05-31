dd6e0c43ef11316cecd1c55ac1d16ec7c8c3165a
Kam Bielawski <kamtb28@gmail.com>
2022-05-31 18:06:01 -0500

    cleanup comments and add ability to save values

--- simulation.py
+++ simulation.py
@@ -13,14 +13,12 @@ class Simulation:
         p.setGravity(0,0,c.GRAVITY_FORCE)
 
     def __del__(self):
+        # self.Save_Values()
         p.disconnect()
 
+    # Run simulation (sense -> act -> update sim) 
     def Run(self, robot):
         self.robot = robot
-        # self.backLegSensorValues = np.zeros(c.TIMESTEPS)
-        # self.frontLegSensorValues = np.zeros(c.TIMESTEPS)
-        # self.targetAngles_front = np.sin(np.linspace(0,2*np.pi, c.TIMESTEPS) * c.FREQUENCY_FRONT + c.PHASE_OFFSET_FRONT)*c.AMPLITUDE_FRONT
-        # self.targetAngles_back = np.sin(np.linspace(0,2*np.pi, c.TIMESTEPS) * c.FREQUENCY_BACK + c.PHASE_OFFSET_BACK)*c.AMPLITUDE_BACK
         for i in range(c.TIMESTEPS):
             if i%100 == 0:
                 print(i)
@@ -30,24 +28,7 @@ class Simulation:
             self.robot.Act(i)
             time.sleep(1/60)
 
-'''
-            # sensor value from robotId
-            self.backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-            self.frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-
-            # simulate a motor for a particular joint
-            pyrosim.Set_Motor_For_Joint(
-                bodyIndex=robotId,
-                jointName="Torso_BackLeg",
-                controlMode=p.POSITION_CONTROL,
-                targetPosition=self.targetAngles_back[i],
-                maxForce=c.MAX_FORCE
-            )
-            pyrosim.Set_Motor_For_Joint(
-                bodyIndex=robotId,
-                jointName="Torso_FrontLeg",
-                controlMode=p.POSITION_CONTROL,
-                targetPosition=self.targetAngles_front[i],
-                maxForce=c.MAX_FORCE
-            )
-'''
+    def Save_Values(self):
+        for sensor in self.robot.sensors:
+            np.save("./data/data_" + self.robot.sensors[sensor].name + ".npy", self.robot.sensors[sensor].values)
+            print("Sensor data saved to ./data/data_" + self.robot.sensors[sensor].name + ".npy")
