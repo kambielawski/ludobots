a57a67a8a3712909218649a01fa917bf9499e07c
Kam Bielawski <kamtb28@gmail.com>
2022-06-14 12:16:42 -0500

    default world.sdf file

--- world.py
+++ world.py
@@ -1,6 +1,11 @@
+import os
 import pybullet as p 
 
 class World:
     def __init__(self, solutionId):
         self.planeId = p.loadURDF("plane.urdf")
-        p.loadSDF("world_" + str(solutionId) + ".sdf")
+        worldFile = "world_" + str(solutionId) + ".sdf"
+        if os.path.exists(worldFile):
+            p.loadSDF(worldFile)
+        else:
+            p.loadSDF("world.sdf")
