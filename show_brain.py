e51caf8c4a8f81e74ec290ceec40076be9b00a2b
kam <kamtb28@gmail.com>
2022-09-12 06:54:09 -0400

    display gui

--- show_brain.py
+++ show_brain.py
@@ -20,5 +20,5 @@ if not os.path.exists(body_file):
     exit(1)
 
 # for windowSize in range(10,500,10):
-os.system("python simulate.py DIRECT 0 " + brain_file + " " + body_file)# " " + str(windowSize) +
+os.system("python simulate.py GUI 0 " + brain_file + " " + body_file)# " " + str(windowSize) +
 
