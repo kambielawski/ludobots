41a45261c6eaef3b9e890d4bc600835aee72a40a
kam <kamtb28@gmail.com>
2022-09-13 09:27:49 -0400

    constants change

--- search.py
+++ search.py
@@ -7,8 +7,8 @@ import constants as c
 
 def Get_Constants_AFPO():
     return {
-        'generations': 1,
-        'population_size': 5
+        'generations': 5,
+        'target_population_size': 10
     }
 
 def Get_Constants_HillClimber():
