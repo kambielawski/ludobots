94560ecaf745564494f2a3cabb29a6a7ac21495f
Kameron Bielawski <kam@EMCFs-MacBook-Pro-3.local>
2023-03-11 09:05:02 -0500

    environment switching

--- experiment.py
+++ experiment.py
@@ -8,24 +8,24 @@ from ageFitnessPareto import AgeFitnessPareto
 
 def Get_Constants_AFPO_T1():
     return {
-        'name': 'tri_emp',
+        'name': 'window_50',
         'generations': 999,
         'target_population_size': 100,
-        'motor_measure': 'VELOCITY', # 'VELOCITY' or 'DESIRED_ANGLE'
+        'motor_measure': 'DESIRED_ANGLE', # 'VELOCITY' or 'DESIRED_ANGLE'
         'objectives': ['box_displacement', 'empowerment'],
-        'empowerment_window_size': c.TIMESTEPS // 2,
+        'empowerment_window_size': 15,
         'batching': False,
         'batch_size': 5
     }
 
 def Get_Constants_AFPO_T2():
     return {
-        'name': 'tri_random',
+        'name': 'window_400',
         'generations': 999,
         'target_population_size': 100,
         'motor_measure': 'DESIRED_ANGLE',
         'objectives': ['box_displacement', 'empowerment'], 
-        'empowerment_window_size': c.TIMESTEPS // 2,
+        'empowerment_window_size': 300,
         'batching': False,
         'batch_size': 5
     }
@@ -47,7 +47,6 @@ class Experiment:
             os.system(f'mkdir {self.experiment_directory}/best_robots/quadruped')
             os.system(f'mkdir {self.experiment_directory}/best_robots/pareto_front')
             os.system(f'cp robots/body_quadruped.urdf {self.experiment_directory}')
-            os.system(f'cp world.sdf {self.experiment_directory}')
 
             # 2. Print experiment information to file
             t1_info = Get_Constants_AFPO_T1()
@@ -56,6 +55,13 @@ class Experiment:
             info_file.write(str(t1_info) + '\n')
             info_file.write(str(t2_info))
             info_file.close()
+
+            # TODO: Generalize world initialization (make per-experiment)
+            # Finish directory setup
+            if 'box_displacement' in t1_info['objectives']:
+                os.system(f'cp ./task_environments/box_world.sdf {self.experiment_directory}/world.sdf')
+            else:
+                os.system(f'cp ./task_environments/world.sdf {self.experiment_directory}')
             
             # 3. Initialize N_runs AFPO objects and pickle them
             treatment_1 = { i: AgeFitnessPareto(t1_info, run_id=(i+1), dir=f'{self.experiment_directory}') for i in range(N_runs) }
