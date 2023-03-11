3df14a6e30a0838ea2cf5ec297e3f403221fb489
Kameron Bielawski <kam@EMCFs-MacBook-Pro-3.local>
2023-03-11 09:22:09 -0500

    search parameters

--- search.py
+++ search.py
@@ -3,10 +3,10 @@ import constants as c
 
 def Get_Constants_AFPO():
     return {
-        'generations': 20,
-        'target_population_size': 50,
-        'motor_measure': 'VELOCITY', # 'VELOCITY' or 'DESIRED_ANGLE'
-        'objectives': ['box_displacement', 'empowerment'], # 'tri_fitness'
+        'generations': 100,
+        'target_population_size': 20,
+        'motor_measure': 'DESIRED_ANGLE', # 'VELOCITY' or 'DESIRED_ANGLE'
+        'objectives': ['empowerment'], # 'tri_fitness'
         'empowerment_window_size': c.TIMESTEPS // 2,
         'batching': True,
         'batch_size': 5
