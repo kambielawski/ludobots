b8136b8110c5c5e5f6deaa34143884adb670c239
Kameron Bielawski <kam@EMCFs-MacBook-Pro-4.local>
2023-05-16 09:15:10 -0400

    Track motor values

--- motor.py
+++ motor.py
@@ -7,8 +7,10 @@ import constants as c
 class Motor:
     def __init__(self, jointName):
         self.jointName = jointName
+        self.values = []
 
     def Set_Value(self, robot, desiredAngle):
+        self.values.append(desiredAngle)
         pyrosim.Set_Motor_For_Joint(
             bodyIndex=robot.robotId,
             jointName=self.jointName,
