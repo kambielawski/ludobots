fda8f13fd3a3f7d5afb80982caf8aac1b0951927
Kameron Bielawski <kam@ip0af5cce7.int.uvm.edu>
2023-09-18 08:32:27 -0400

    consistent cleaning

--- scripts/clean_dir.py
+++ scripts/clean_dir.py
@@ -8,7 +8,12 @@ args = parser.parse_args()
 
 OS_RM = 'del' if platform == 'win32' else 'rm'
 # Remove the rest
-os.system(OS_RM + f' {args.dir}/world_*.sdf')
-os.system(OS_RM + f' {args.dir}/brain_*.nndf')
 os.system(OS_RM + f' {args.dir}/body_*_*.urdf')
 os.system(OS_RM + f' {args.dir}/fitness_*.txt')
+
+for i in range(10):
+    try:
+        os.system(OS_RM + f' {args.dir}/world_{i}*.sdf')
+        os.system(OS_RM + f' {args.dir}/brain_{i}*.nndf')
+    except:
+        print('Something went wrong')
