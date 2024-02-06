6114d22f372d683fcf5c36b7b0ebfced42a29efa
Kameron Bielawski <kam@ip0af5cc03.int.uvm.edu>
2024-02-06 10:38:41 -0500

    divide evo runs into separate trial objects

--- experiment.py
+++ experiment.py
@@ -5,6 +5,54 @@ import numpy as np
 
 from ageFitnessPareto import AgeFitnessPareto
 
+class Trial:
+    """Trial class
+    - Initialized when an experiment is submitted
+    - Creates and manages experiment directory
+    - Has access to the AFPO-level pickle file
+    """
+    def __init__(self, run_idx, experiment_directory, experiment_parameters):
+        self.experiment_directory = experiment_directory
+        self.trial_directory = self.experiment_directory + f'/trial_{run_idx}'
+        self.pickle_file = f'{self.trial_directory}/trial_{run_idx}.pkl'
+
+        self.afpo = AgeFitnessPareto(experiment_parameters, run_id=run_idx, dir=f'{self.experiment_directory}/trial_{run_idx}')
+        self.max_generations = experiment_parameters['generations']
+        self.current_generation = 0
+        # Create trial-level pickle file for AFPO object
+        with open(self.pickle_file, 'wb') as pickle_file:
+            pickle.dump(self.afpo, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def Run(self):
+        """Run the different runs until we max out"""
+        while self.current_generation < self.max_generations:
+            self.Run_One_Generation()
+
+    def Run_One_Generation(self):
+        """Run a single generation of the trial"""
+        t_start = time.time()
+        # 1. Unpickle previous generation
+        with open(self.pickle_file, 'rb') as pickle_file:
+            self.afpo = pickle.load(pickle_file)
+            if not isinstance(self.afpo, AgeFitnessPareto):
+                raise TypeError("Pickled object needs to be of type AgeFitnessPareto")
+
+        # Save population pickle file (for insurance)
+        os.system(f'cp {self.trial_directory}/{self.pickle_file} {self.trial_directory}/saved_{self.pickle_file}')
+        
+        # 2. Compute a single generation for this trial
+        print(f'\n\n========== \n Generation {self.afpo.currentGen} - Run {self.afpo.run_id} \n ==========\n\n')
+        self.afpo.Evolve_One_Generation()
+        self.afpo.Clean_Directory()
+
+        # 3. Pickle runs
+        with open(self.pickle_file, 'wb') as pkl:
+            pickle.dump(self.afpo, pkl, protocol=pickle.HIGHEST_PROTOCOL)
+
+        t_end = time.time()
+        self.one_gen_time = t_end - t_start
+
+
 class Experiment:
     """Experiment class
     - Initialized when an experiment is submitted
@@ -13,55 +61,30 @@ class Experiment:
     def __init__(self, experiment_directory='.', exp_file=None):
         if experiment_directory: # Continue existing experiment
             #TODO: error checking on directory status
-            self.pickle_file = f'{experiment_directory}/evo_runs.pickle'
+            self.pickle_file = f'{experiment_directory}/experiment.pkl'
             self.experiment_directory = experiment_directory
+
+            with open(self.pickle_file, 'rb') as pkl:
+                self.experiment_object = pickle.load(pkl, 'rb')
+                self.experiment_directory = self.experiment_object['experiment_directory']
+                self.trials = self.experiment_object['trials']
+                self.experiment_params = self.experiment_object['experiment_parameters']
         else: # Initialize a new experiment
             self.initialize_directory(exp_file)
 
     def Run(self):
         """Run the different runs until we max out"""
-        while self.current_generation < self.max_generations:
-            self.Run_One_Generation()
+        for run_idx, trial in self.trials.items():
+            trial.Run()
 
     def Run_Vacc(self):
         # Submit a job for each trial... 
         # all the trials use the exact same exp params
         # Each job will be a run_exp.py (NOT run_exp_vacc.py) with the
         # TRIAL directory as the argument for exp_directory
-        pass
-
-    def Run_One_Generation(self):
-        t_start = time.time()
-        # 1. Unpickle previous generation
-        with open(self.pickle_file, 'rb') as pickle_file:
-            self.pkl_obj = pickle.load(pickle_file)
-            if isinstance(self.pkl_obj, dict):
-                self.evo_runs = self.pkl_obj
-            elif isinstance(self.pkl_obj, Experiment):
-                self.evo_runs = self.pkl_obj.evo_runs
-            else:
-                raise TypeError("Pickled object needs to be of type Experiment or type dictionary")
-
-        # Save population pickle file (for insurance)
-        os.system(f'cp {self.experiment_directory}/evo_runs.pickle {self.experiment_directory}/evo_runs_saved.pickle')
-        
-        # 2. Compute a single generation for all runs
-        for treatment in self.evo_runs: 
-            for run in self.evo_runs[treatment]:
-                # Create a directory for the *active* pareto front if it doesn't exist yet 
-                if not os.path.exists(f'{self.experiment_directory}/pareto_front/run_{self.evo_runs[treatment][run].run_id}'):
-                    os.system(f'mkdir {self.experiment_directory}/pareto_front/run_{self.evo_runs[treatment][run].run_id}')
-                print(f'\n\n========== \n Generation {self.evo_runs[treatment][run].currentGen} - Run {run} \n ==========\n\n')
-                self.evo_runs[treatment][run].Evolve_One_Generation()
-                self.evo_runs[treatment][run].Clean_Directory() # Clean up experiment directory
-
-        # 3. Pickle runs
-        with open(self.pickle_file, 'wb') as pklFileHandle:
-            pickle.dump(self.evo_runs, pklFileHandle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        t_end = time.time()
-        self.one_gen_time = t_end - t_start
-        self.Print_GenTime_To_File()
+        for trial in self.trials:
+            # submit VACC job
+            pass
 
     def initialize_directory(self, exp_file):
           # 1. Create a new experiment directory
@@ -93,19 +116,17 @@ class Experiment:
           param_file.write(str(experiment_parameters) + '\n')
           param_file.close()
 
-          # 3. Copy experiment parameters for 1 trial
-          param_file_1trial = open(f'{self.experiment_directory}/exp_params_1trial.txt', 'w')
+          # 3. Copy experiment parameters for 1 trial into trial directories
           exp_params_1trial = experiment_parameters
           exp_params_1trial['n_trials'] = 1
-          param_file_1trial.write(str(exp_params_1trial) + '\n')
-          param_file.close()
-
-          # 4. Copy the experiment params to the trial directories
+          
           for run_idx in range(self.n_runs):
-              os.system(f'mkdir {self.experiment_directory/trial_{run_idx}}')
-              os.system(f'cp {self.experiment_directory}/exp_params_1trial.txt {self.experiment_directory}/trial_{run_idx}')
+              os.system(f'mkdir {self.experiment_directory}/trial_{run_idx}')
+              param_file_1trial = open(f'{self.experiment_directory}/trial_{run_idx}/params_trial_{run_idx}.txt', 'w')
+              param_file_1trial.write(str(exp_params_1trial) + '\n')
+              param_file.close()
 
-          # Error checking for proper task environment 
+          # Error checking for proper task environment
           if 'boxdisplacement' in np.array([sim['objectives'] for sim in experiment_parameters['simulations']]).flatten() and experiment_parameters['task_environment'] != './task_environments/box_world.sdf':
               raise ValueError('Check your experiment setup. Cannot select for box displacement if box_world.sdf is not the task environment.')
           
@@ -115,14 +136,19 @@ class Experiment:
               task_env_file_name = sim['task_environment'].split('/')[2]
               template_env_file = sim['task_environment']
               os.system(f'cp {template_env_file} {self.experiment_directory}/{task_env_file_name}')
+              for run_idx in range(self.n_runs):
+                  os.system(f'cp {template_env_file} {self.experiment_directory}/trial_{run_idx}/{task_env_file_name}')
           
-          # 5. Initialize n_runs AFPO objects and pickle them in their respective trial directories
-          for run_idx in range(self.n_runs):
-              run_dict = {0: AgeFitnessPareto(exp_params_1trial, run_id=(run_idx+1), dir=f'{self.experiment_directory}/trial_{run_idx}')}
-              pickle_file = f'{self.experiment_directory}/trial_{run_idx}/trial_{run_idx}.pkl'
-              with open(pickle_file, 'wb') as pklFileHandle:
-                  pickle.dump(run_dict, pklFileHandle)
-
+          # 5. Initialize n_runs Trial objects and pickle them into an aggregate trial directories
+          self.trials = { run_idx: Trial(run_idx, self.experiment_directory, exp_params_1trial) for run_idx in range(self.n_runs) }
+          self.experiment_params = experiment_parameters
+          experiment_object = {
+              'trials': self.trials,
+              'experiment_parameters': experiment_parameters,
+              'experiment_directory': self.experiment_directory
+          }
+          with open(f'{self.experiment_directory}/experiment.pkl', 'wb') as pkl:
+              pickle.dump(experiment_object, pkl)
       
     def Get_Experiment_Parameters(self, experiment_file):
         expfile = open(experiment_file)
