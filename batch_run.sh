f704f41f1c9f7f75ca3ba5904f9cb158efb65897
Kameron Bielawski <ktbielaw@vacc-user1.cluster>
2024-02-09 11:26:08 -0500

    update sbatch script

--- batch_run.sh
+++ batch_run.sh
@@ -15,4 +15,4 @@ cd /gpfs1/home/k/t/ktbielaw/projects/ludobots
 
 source /gpfs1/home/k/t/ktbielaw/anaconda3/bin/activate pyrosim
 
-python3 run_exp.py --exp $3 --vacc
+python3 run_exp.py --exp $1 --vacc
